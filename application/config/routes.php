c4c54ee3e0126649ec6f17fe80803e389f4967f1
Mike Lewis <mike_lewis_@hotmail.com>
2016-01-28 14:09:40 -0800

    fixed the seventh link, changed the changelog

--- application/config/routes.php
+++ application/config/routes.php
@@ -44,6 +44,7 @@ $route['first'] = 'first';
 $route['last'] = 'last/welcome';
 $route['(:any)'] = "first/$1";
 $route['lock/(:any)/(:any)'] = "welcome/shucks";
+$route['([a-zA-Z]{4,4})/bingo'] = 'bingo'; // Regular expression matches exactly 4 chars (upper or lower)
 
 
 
